b6f35a328c23e5970abfb93e979b2bc3f39fe153
sunil <skhatri15@outlook.com>
2024-07-29 01:10:52 -0400

    added todo files for installation and uninstallation scripts to remove manually setting env, alias, and symlink

---
+++ cdh_install.sh
@@ -0,0 +1,4 @@
+#TODO: Set alias here
+#TODO: export CD_HISTORY_PATH here
+#TODO: MAYBE create symlinks into ~/.local/bin from here
+#TODO: Update README instructions to instruct sourcing this installation file instead of setting env variable and alias manually
---
+++ cdh_uninstall.sh
@@ -0,0 +1,4 @@
+#TODO: Revert install by removing:
+#* alias cd
+#* $CD_HISTORY_PATH
+#* removing symlinks
