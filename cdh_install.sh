86a3572ac4684ccc6f0ba22061381d2e94f7fe40
sunil <skhatri15@outlook.com>
2024-07-31 17:53:44 -0400

    removing files that should've only been in feature_installation branch for now

--- cdh_install.sh
+++
@@ -1,5 +0,0 @@
-#TODO: MAYBE provide options for default locations, or an option to provide directory to symlink into
-#TODO: Set alias here
-#TODO: export CD_HISTORY_PATH here
-#TODO: MAYBE create symlinks into ~/.local/bin from here
-#TODO: Update README instructions to instruct sourcing this installation file instead of setting env variable and alias manually
--- cdh_uninstall.sh
+++
@@ -1,4 +0,0 @@
-#TODO: Revert install by removing:
-#* alias cd
-#* $CD_HISTORY_PATH
-#* removing symlinks
